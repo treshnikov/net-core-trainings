1d1eb1c7482b6e852d140f8bf43f734ad1a80b31
pavel.treshnikov <treshnikov@gmail.com>
2021-01-04 23:45:09 +0400

    Add secure client

---
+++ src/SecureClient/AuthConfig.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Globalization;
+using Microsoft.Extensions.Configuration;
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+    public string Instance { get; set; }
+    public string TenantId { get; set; }
+    public string ClientId { get; set; }
+    public string Authority {
+        get
+        {
+            return string.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+        }
+    }
+    public string ClientSecret { get; set; }
+    public string BaseAddress { get; set; }
+    public string ResourceId { get; set; }
+
+    public static AuthConfig ReadFileFromJson(string path)
+    {
+        IConfiguration Configuration;
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+        Configuration = builder.Build();
+
+        return Configuration.Get<AuthConfig>();
+
+    }
+
+    }
+}
---
+++ src/SecureClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecureClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
